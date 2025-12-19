b31914b4fff80b60cde2fec1341b7ca0e0d8565f
Akcarc <babluverma636813@gmail.com>
2025-12-19 21:24:53 +0530

    Change to hover

--- src/Component.jsx/ToggleButton.jsx
+++ src/Component.jsx/ToggleButton.jsx
@@ -4,14 +4,14 @@ import TriggerContext from "../store/ToggleContext";
 
 export default function TriggerToggle() {
   const { trigger, setTrigger } = useContext(TriggerContext);
-
+ 
   return (
     <div className="flex gap-4 items-center mb-4">
       <span className="font-medium">Trigger:</span>
-
+      <div className="border  rounded-md">
       <button
         onClick={() => setTrigger("click")}
-        className={`px-3 py-1 border ${
+        className={`px-3 py-1 border rounded-l-md ${
           trigger === "click" ? "bg-black text-white" : "bg-white"
         }`}
       >
@@ -20,12 +20,13 @@ export default function TriggerToggle() {
 
       <button
         onClick={() => setTrigger("hover")}
-        className={`px-3 py-1 border ${
+        className={`px-3 py-1 border rounded-r-md ${
           trigger === "hover" ? "bg-black text-white" : "bg-white"
         }`}
       >
         Hover
       </button>
+      </div>
     </div>
   );
 }
